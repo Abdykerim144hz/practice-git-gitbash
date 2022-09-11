39cfea37130f61b76bc27377cd18cecbbf9dd5fe
Aeby Stonks <daniarutub@gmail.com>
2022-09-11 21:19:16 +0600

    firstCommit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+let newUser = prompt("Are u realy interesting programming?")
+if (newUser == "Yes" || newUser == "yes") {
+	alert("Welcome")
+}
+
+else if (newUser == null || newUser == "") {
+	alert("canceled")
+}
+
+else {
+	alert("get the fuck out");
+}
